32aabb36584322aade9200c1d6f5885ec920775d
gabriel-adutra <gabriel_dutra497@hotmail.com>
2020-09-17 18:47:28 -0300

    Corrigindo códigos das semanas 2 e 4

--- Using Python to Access Web Data/week4.2.py
+++ Using Python to Access Web Data/week4.2.py
@@ -7,11 +7,15 @@ ctx = ssl.create_default_context()
 ctx.check_hostname = False
 ctx.verify_mode = ssl.CERT_NONE
 
-url = input('Enter - ')
-html = urllib.request.urlopen(url, context=ctx).read()
-soup = BeautifulSoup(html, 'html.parser')
 
-# Retrieve all of the anchor tags
-tags = soup('a')
-for tag in tags:
-    print(tag.get('href', None))
+URL = input("Enter the URL:") #Enter main URL
+posicao = int(input("Enter position:")) - 1 #The position of link relative to first link
+count = int(input("Enter count:")) #The number of times to be repeated
+
+while count >= 0:
+    html = urllib.request.urlopen(URL, context=ctx).read()
+    soup = BeautifulSoup(html)
+    tags = soup('a')
+    print(URL)
+    URL = tags[posicao].get("href", None)
+    count = count - 1
